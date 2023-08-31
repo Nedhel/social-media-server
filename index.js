d8a3a2af0ecfe8c7a9340764cb261e3424ebb78e
Tony <qkrtnqja97@gmail.com>
2023-08-30 17:49:20 -0700

    Change userDataname -SB

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const {
 
 //Solicitud previa
 app.options("/", (req, res) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "http://localhost:5000");
     res.header("Access-Control-Allow-Headers", "task"); // Allow the 'task 'header
     res.header("Access-Control-Allow-Methods", "GET"); // Allow the GET method
     res.header("Access-Control-Allow-Methods", "POST"); // Allow the POST method
